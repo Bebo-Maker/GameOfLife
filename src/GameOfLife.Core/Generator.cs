69919945bf6585b0fd451a0b815c107bd73bb499
Bebo-Maker <bennetbo@gmx.de>
2021-11-02 22:33:50 +0100

    Create Generator.cs

---
+++ src/GameOfLife.Core/Generator.cs
@@ -0,0 +1,19 @@
+﻿namespace GameOfLife.Core
+{
+  public static class Generator
+  {
+    private static readonly Random Random = new();
+    
+    public static Cell[,] RandomCells(int cols, int rows)
+    {
+      var cells = new Cell[cols, rows];
+      for (int x = 0; x < cols; x++)
+        for (int y = 0; y < rows; y++)
+          cells[x, y] = RandomCell();
+
+      return cells;
+    }
+    
+    public static Cell RandomCell() => Random.Next(0, 3) == 0 ? Cell.Alive : Cell.Dead;
+  }
+}
