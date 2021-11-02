3fd2c61f3af40348711b71742fe18b41fb857467
Bebo-Maker <bennetbo@gmx.de>
2021-11-02 22:33:07 +0100

    Create Position.cs

---
+++ src/GameOfLife.Core/Position.cs
@@ -0,0 +1,17 @@
+﻿namespace GameOfLife.Core;
+
+public struct Position
+{
+  public int X { get; }
+  public int Y { get; }
+
+  public Position(int x, int y)
+  {
+    X = x;
+    Y = y;
+  }
+
+  public (int X, int Y) Deconstruct() => (X, Y);
+
+  public static implicit operator Position((int X, int Y) d) => new(d.X, d.Y);
+}
