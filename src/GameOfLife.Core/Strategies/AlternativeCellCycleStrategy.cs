4a72103309ac6562d33b50b15844697b4110e9d8
Bebo-Maker <Bebo-Maker@users.noreply.github.com>
2021-11-03 22:53:56 +0100

    Create AlternativeCellCycleStrategy.cs

---
+++ src/GameOfLife.Core/Strategies/AlternativeCellCycleStrategy.cs
@@ -0,0 +1,6 @@
+﻿namespace GameOfLife.Core.Strategies;
+
+public sealed class AlternativeCellCycleStrategy : ICellCycleStrategy
+{
+  public Cell Update(Cell cell, int aliveNeighbourCount) => aliveNeighbourCount % 2 == 0 ? Cell.Dead : Cell.Alive;
+}
