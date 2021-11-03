cf2ca35ec9bf00e3a66b4ba61e91bad1052c9ed4
Bebo-Maker <Bebo-Maker@users.noreply.github.com>
2021-11-03 22:55:01 +0100

    Add base class for implementing disposable.

---
+++ src/GameOfLife.Desktop/Disposable.cs
@@ -0,0 +1,31 @@
+﻿namespace GameOfLife.Desktop;
+
+public class Disposable : IDisposable
+{
+  protected bool IsDisposed { get; private set; }
+
+  ~Disposable()
+  {
+    Dispose(false);
+  }
+
+  public void Dispose()
+  {
+    Dispose(true);
+    GC.SuppressFinalize(this);
+  }
+
+  private void Dispose(bool disposing)
+  {
+    if (IsDisposed)
+      return;
+
+    if (disposing)
+      DisposeManagedResources();
+
+    DisposeUnmanagedResources();
+  }
+
+  protected virtual void DisposeManagedResources() { }
+  protected virtual void DisposeUnmanagedResources() { }
+}
